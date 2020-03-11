c19c81dc962e4f306dc9e97b6d393b526dc04ef6
i-lab user <i-labuser@SDPL88534-iMacProLO.local>
2020-03-10 18:27:18 -0700

    i finish stuff

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -9,17 +9,33 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
-		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println();
 
-		// 1. Use each value of randomNumber to give the user a random compliment.
-if(==)
-		// 2. Repeat all the code above 10 times
-		
-		// 3. Find someone to test out your program. They will like it :)
+		Random randomMaker = new Random();
+
+		for (int i = 0; i < 10; i++) {
+
+			int randomNumber = randomMaker.nextInt(5);
+
+			System.out.println();
+
+			// 1. Use each value of randomNumber to give the user a random compliment.
+
+			// 2. Repeat all the code above 10 times
+
+			// 3. Find someone to test out your program. They will like it :) are you sure
+			// about that
+
+			if (randomNumber == 0) 
+				JOptionPane.showMessageDialog(null, "you have nice hair");
+			if (randomNumber == 4) 
+				JOptionPane.showMessageDialog(null, "you smell great");
+			if (randomNumber == 2)
+				JOptionPane.showMessageDialog(null, "you are so smart");
+			if (randomNumber == 1)
+				JOptionPane.showMessageDialog(null, "you are super nice");
+			if (randomNumber == 3)
+				JOptionPane.showMessageDialog(null, "you are helpful");
+		}
+
 	}
 }
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -6,30 +6,46 @@ package _05_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
+public static void main(String[] args) {
+	
 
 	// 1. Make a main method that includes all the steps below….
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+Random randy=new Random();
+int num=randy.nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(num);
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog(null,"enter a question");
 	// 5. If the random number is 0
+if(num==0) {
+	JOptionPane.showMessageDialog(null,"dont ask me ask your mom");
+}
 
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
-
+if(num==1) {
+	JOptionPane.showMessageDialog(null,"try again");
+}
 	// -- tell the user "No"
 
 	// 7. If the random number is 2
 
 	// -- tell the user "Maybe you should ask Google?"
+if(num==2) {
+	JOptionPane.showMessageDialog(null,"to addicted google is smatter then you ask google please");
+}
 
 	// 8. If the random number is 3
-
+if(num==3) {
+	JOptionPane.showMessageDialog(null,"the answer was a answer");
+}
+	
 	// -- write your own answer
-
+}
 }
